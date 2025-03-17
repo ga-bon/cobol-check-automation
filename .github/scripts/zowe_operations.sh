622bd8d106eb219b8ea53739749d94d6c781a37a
ga-bon <gaboar47@gmail.com>
2025-03-17 17:54:48 -0600

    Update zowe_operations.sh version16

--- .github/scripts/zowe_operations.sh
+++ .github/scripts/zowe_operations.sh
@@ -12,7 +12,7 @@ echo "Directory already exists."
 fi
 # Upload files
 zowe zos-files upload dir-to-uss "./cobol-check" "/z/$LOWERCASE_USERNAME/cobolcheck" --recursive
---binary-files "cobol-check-0.2.9.jar"
+--binary-files "cobol-check-0.2.16.jar"
 # Verify upload
 echo "Verifying upload:"
 zowe zos-files list uss-files "/z/$LOWERCASE_USERNAME/cobolcheck"
